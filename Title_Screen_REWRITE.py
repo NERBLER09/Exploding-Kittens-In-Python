735544e58e2538a4b69cc48bec426cd36ba8f623
NERBLER09 <noah.beaudin1@gmail.com>
2020-08-28 08:58:23 -0700

    Fixed Minor Bugs

--- Title_Screen_REWRITE.py
+++ Title_Screen_REWRITE.py
@@ -1,5 +1,6 @@
 # imports all the modules needed 
 import tkinter as tk
+from tkinter import messagebox
 import webbrowser
 
 # Creates the Tk Window
@@ -11,7 +12,7 @@ windows.resizable(0, 0) # Makes so the window is not resizable
 
 # Functions needed 
 def play_button_clicked():
-    play = tk.messagebox.askquestion("Exploding Kittens Main Menu", "The game is very incomplete, are you sure you want to continue?")
+    play = messagebox.askquestion("Exploding Kittens Main Menu", "The game is very incomplete, are you sure you want to continue?")
 
     if play == "yes":
         windows.destroy()
@@ -30,7 +31,7 @@ def web_clicked():
 
 def show_about():
     # Shows the about messagebox
-    tk.messagebox.showinfo("About", "Exploding Kittens Game, recreated in python by Noah Beaudin\nVersos: Alpha Build 1\nGithub Page: github.com/nerbler09\nCheck the licence for sharing and use of the source code")
+    messagebox.showinfo("About", "Exploding Kittens Game, recreated in python by Noah Beaudin\nVersos: Alpha Build 1\nGithub Page: github.com/nerbler09\nCheck the licence for sharing and use of the source code")
 
 def show_licence_file():
     # Opens the Licence.rft File In Wordpad
