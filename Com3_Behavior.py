751e64f7b4c0d2a3ebfbf043471c2a86787f5c42
Noah Beaudin <noah.beaudin1@gmail.com>
2020-10-13 17:54:47 -0600

    Made com 3 decide a card that there going to play

--- Com3_Behavior.py
+++ Com3_Behavior.py
@@ -1,6 +1,15 @@
 import Game_REWRITE
 from tkinter import messagebox
+import Deal_To_Com_Players as DTCP
+import random
 
+# Main function that decides the card com 3 should play
 def decied_card_to_play():
     if Game_REWRITE.com3_turn == True:
         messagebox.showinfo("Exploding Kittens Game", "It is currently com 3's turn.")
+
+        print("Com 3's hand: " + str(DTCP.com3_cards))
+
+        card_to_play = random.choice(DTCP.com2_cards)
+
+        print("Card Com 3's going to play " + str(card_to_play))
