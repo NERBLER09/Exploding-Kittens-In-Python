c7fe0a6956c70475c50975819f8b7fde9c40f945
Noah Beaudin <noah.beaudin1@gmail.com>
2020-10-24 11:07:49 -0600

    Updated when com 3 has played a skip or attack

--- Check_Com2_Card_Played.py
+++ Check_Com2_Card_Played.py
@@ -367,20 +367,38 @@ def check_card_played():
     # Checks if com 2 has played an skip card
     if C2B.card_to_play == "skip":
         time.sleep(1)
-        messagebox.showinfo("Exploding Kitens Game", "Com 2 has skiped there turn, it's now currently your turn")
-        Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
-        Game_REWRITE.player_turn = True
-        Game_REWRITE.com1_turn = False
-        Game_REWRITE.com2_turn = False
+        
+        if wt.com_number == 2:    
+            messagebox.showinfo("Exploding Kitens Game", "Com 2 has skiped there turn, it's now currently your turn")
+            Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
+            Game_REWRITE.player_turn = True
+            Game_REWRITE.com1_turn = False
+            Game_REWRITE.com2_turn = False
+        
+        else:
+            messagebox.showinfo("Exploding Kitens Game", "Com 2 has skiped there turn, it's now com 3's turn")
+            Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
+            Game_REWRITE.player_turn = True
+            Game_REWRITE.com1_turn = False
+            Game_REWRITE.com2_turn = False
 
     # Checks if com 2 has played an attack card
     elif C2B.card_to_play == "attack":
         time.sleep(1)
-        messagebox.showinfo("Exploding Kittens Game", "Com 2 has played an attack. Com 2 has skiped there turn and made you have 2 turns")
-        Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
-        Game_REWRITE.player_turn = True
-        Game_REWRITE.com1_turn = False
-        Game_REWRITE.com2_turn = False
+
+        if wt.com_number == 2:
+            messagebox.showinfo("Exploding Kittens Game", "Com 2 has played an attack. Com 2 has skiped there turn and made you have 2 turns")
+            Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
+            Game_REWRITE.player_turn = True
+            Game_REWRITE.com1_turn = False
+            Game_REWRITE.com2_turn = False
+
+        else:
+            messagebox.showinfo("Exploding Kittens Game", "Com 2 has played an attack. Com 2 has skiped there turn and made com 3 have 2 turns")
+            Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
+            Game_REWRITE.player_turn = True
+            Game_REWRITE.com1_turn = False
+            Game_REWRITE.com2_turn = False
     
     # Checks if com 2 has played an favor card
     elif C2B.card_to_play == "favor":
