77e8cc8676ac6145d481f2a0dba3b6f48718cee1
Noah Beaudin <noah.beaudin1@gmail.com>
2020-10-24 19:57:47 -0600

    Added in some missing comments

--- Check_Com2_Card_Played.py
+++ Check_Com2_Card_Played.py
@@ -386,6 +386,7 @@ def check_card_played():
     elif C2B.card_to_play == "attack":
         time.sleep(1)
 
+        # Checks if the player selected 2 com players
         if wt.com_number == 2:
             messagebox.showinfo("Exploding Kittens Game", "Com 2 has played an attack. Com 2 has skiped there turn and made you have 2 turns")
             Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
@@ -394,6 +395,7 @@ def check_card_played():
             Game_REWRITE.com2_turn = False
 
         else:
+            # Tells that com 2 has played an attack card and make com 3 have 2 turns
             messagebox.showinfo("Exploding Kittens Game", "Com 2 has played an attack. Com 2 has skiped there turn and made com 3 have 2 turns")
             Game_REWRITE.discard_pile_text.set(C2B.card_to_play + "\n\n\n")
             Game_REWRITE.player_turn = True
